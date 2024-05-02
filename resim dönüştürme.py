bcbbb5818c40cd5cb62946e4e11c3a0e97d0194f
Mehmet AKSAN <133366485+RuzgarV@users.noreply.github.com>
2024-05-02 16:40:59 +0300

    image conversion.py

--- resim dönüştürme.py
+++ resim dönüştürme.py
@@ -1,10 +1,7 @@
 from PIL import Image
 
-# Resmi yükle
 image = Image.open('Adli4.jpg')
 
-# Resmi 28x28 boyutuna yeniden boyutlandır
 resized_image = image.resize((28, 28))
 
-# Sonucu kaydet
 resized_image.save('mnist_formatinda_resim.jpg')
